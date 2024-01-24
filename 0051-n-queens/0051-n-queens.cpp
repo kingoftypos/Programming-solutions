1e0d38f7ff53d0707fa80b2ef8c4ac5966e09f76
Shreyansh <105883701+kingofypos@users.noreply.github.com>
2024-01-24 15:48:42 +0530

    Time: 6 ms (59.22%), Space: 9.3 MB (24.32%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,41 @@
+class Solution {
+    public:
+    void solve(int col,vector<vector<string>> &ans,vector<string> &board,int n,vector<int> &ud,vector<int> &ld,vector<int> &leftr)
+    {
+        if(col==n)
+        {
+            ans.push_back(board);
+            return;
+        }
+        for(int row=0;row<n;row++)
+        {
+            if((ud[n-1+col-row]==0) && ld[row+col]==0 && leftr[row]==0)
+            {
+                board[row][col]='Q';
+               ud[n-1+col-row]=1;
+               ld[row+col]=1;
+               leftr[row]=1;
+               solve(col+1,ans,board,n,ud,ld,leftr);
+               board[row][col]='.';
+               ud[n-1+col-row]=0;
+               ld[row+col]=0;
+               leftr[row]=0;
+            }
+
+        }
+    }
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++)
+        board[i]=s;
+        vector<int> ud(2*n-1,0);
+        vector<int> ld(2*n-1,0);
+        vector<int> leftr(n,0);
+        solve(0,ans,board,n,ud,ld,leftr);
+       return ans;
+        
+    }
+};
