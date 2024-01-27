81a2815ce6ed7c6dfca588081af8f7d2d6e7730b
Shreyansh <105883701+kingofypos@users.noreply.github.com>
2024-01-27 23:10:52 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (8.20%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        vector<int> arr;
+        int fact=1;
+        for(int i=1;i<n;i++)
+        {
+            fact*=i;
+            arr.push_back(i);
+            
+        }
+        arr.push_back(n);
+        k-=1;
+        string ans="";
+        while(true)
+        {
+            ans+= to_string(arr[k/fact]);
+            arr.erase(arr.begin()+ (k/fact));
+            if(arr.size()==0)
+            break;
+            
+            k=k%fact;
+            fact=fact/arr.size();
+        }
+        return ans;
+
+        
+    }
+};
