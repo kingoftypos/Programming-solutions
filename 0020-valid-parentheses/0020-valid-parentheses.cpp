7aa42211cf7cd00cf69fd41828b8b5e1e5a0c0ad
Shreyansh <105883701+kingoftypos@users.noreply.github.com>
2024-02-11 16:13:48 +0530

    Time: 0 ms (100%), Space: 7.5 MB (20.85%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> temp;
+        stack<char> st;
         for(int i=0;i<s.size();i++)
         {
             if(s[i]=='(' || s[i]=='{' || s[i]=='[')
             {
-                temp.push(s[i]);
+                st.push(s[i]);
             }
-            else if(s[i]==')' && temp.top()=='(')
-            temp.pop();
-            else if(s[i]=='}' &&  temp.top()=='{')
-            temp.pop();
-            else if(s[i]==']' && temp.top()=='[')
-            temp.pop();
-            else
-            return false;
+           else
+           {
+               if(st.empty())
+               return 0;
+               else
+               {
+                   if(s[i]=='}' && st.top()=='{')
+                   st.pop();
+                   else if(s[i]==')' && st.top()=='(')
+                   st.pop();
+                   else if(s[i]==']' && st.top()=='[')
+                   st.pop();
+                   else
+                   return 0;
+               }
+           }
+         
 
         }
-        return true;
+       return st.empty();
         
     }
 };
