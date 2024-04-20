ece995e119083009dd7e30c45e266144b5002ae6
Shreyansh <105883701+kingoftypos@users.noreply.github.com>
2024-04-20 22:54:16 +0530

    Time: 58 ms (41.87%), Space: 38 MB (22.58%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<vector<int>> ans(n,vector<int>(m,0));
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        vector<int> x={-1,0,1,0};
+        vector<int> y={0,1,0,-1};
+        queue<pair<pair<int,int>,int>> q;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(mat[i][j]==0)
+                {
+                    vis[i][j]=1;
+                    q.push({{i,j},0});
+                    ans[i][j]=0;
+                }
+               
+            }
+        }
+        while(!q.empty())
+        {
+            int row=q.front().first.first;
+            int col=q.front().first.second;
+            int steps=q.front().second;
+            q.pop();
+           ans[row][col]=steps;
+            for(int i=0;i<4;i++)
+            {
+                int nrow=row+x[i];
+                int ncol=col+y[i];
+                if(nrow>=0 && ncol>=0 &&nrow<n && ncol<m && !vis[nrow][ncol]){
+                vis[nrow][ncol]=1;
+                
+                q.push({{nrow,ncol},steps+1});
+                }
+            }
+            
+        }
+     return ans;   
+    }
+};
