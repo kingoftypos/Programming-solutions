18f997f85eed778f956ae9d2ffa20aee3ca20730
Shreyansh <105883701+kingoftypos@users.noreply.github.com>
2024-07-31 02:36:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,44 +1,40 @@
 class Solution {
 public:
-    vector<int> findOrder(int nc, vector<vector<int>>& pr) {
-        vector<int> adj[nc];
-        for(int i=0;i<pr.size();i++)
+    vector<int> findOrder(int n, vector<vector<int>>& pre) {
+        vector<int> vis(n,0);
+        queue<int> q;
+        vector<int> adj[n];
+        vector<int> topo;
+        vector<int> indegree(n,0);
+        for(int i=0;i<pre.size();i++)
         {
-            int u=pr[i][0];
-            int v=pr[i][1];
-            adj[v].push_back(u);
-            
-         }
-        vector<int> indegree(nc,0);
-        for(int i=0;i<nc;i++)
+            adj[pre[i][1]].push_back(pre[i][0]);
+        }
+        for(int i=0;i<n;i++)
         {
-            for(auto it:adj[i])
+            for(int it:adj[i])
             {
                 indegree[it]++;
             }
         }
-        queue<int> q;
-        for(int i=0;i<nc;i++)
+        for(int i=0;i<n;i++)
         {
-            if(indegree[i]==0)
-           q.push(i);
+            if(!indegree[i])
+            q.push(i);
         }
-        vector<int> ans;
         while(!q.empty())
         {
             int node=q.front();
             q.pop();
-            ans.push_back(node);
-            for(auto it:adj[node])
+            topo.push_back(node);
+            for(int it:adj[node])
             {
                 indegree[it]--;
                 if(indegree[it]==0)
                 q.push(it);
             }
         }
-        return ans.size()==nc?ans : vector<int>{};
-        
-
+        return topo;
         
     }
 };
