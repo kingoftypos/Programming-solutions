ddcbc93da21f4580b4fb0c3d737df5408e6402b5
Shreyansh <105883701+kingoftypos@users.noreply.github.com>
2024-08-07 17:44:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-void help(int i,set<vector<int>> &st,vector<int> &temp,vector<int> nums)
+void help(int i,vector<vector<int>> &ans,vector<int> &temp,vector<int> nums)
         {
             if(i==nums.size())
             {
-                st.insert(temp);
+                if(ans.empty() || ans.back()!=temp)
+                ans.push_back(temp);
                 return;
             }
             temp.push_back(nums[i]);
-            help(i+1,st,temp,nums);
+            help(i+1,ans,temp,nums);
             temp.pop_back();
-            help(i+1,st,temp,nums);
+            help(i+1,ans,temp,nums);
         }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> temp;
-        set<vector<int>> st;
+        
         sort(nums.begin(),nums.end());
-        help(0,st,temp,nums);
-        for(auto it: st)
-        ans.push_back(it);
+        help(0,ans,temp,nums);
+        
         return ans;
         
     }
