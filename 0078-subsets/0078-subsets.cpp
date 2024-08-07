f356ecd62bcdbf4c0b453a385249fbaafa2dfad5
Shreyansh <105883701+kingoftypos@users.noreply.github.com>
2024-08-07 16:36:43 +0530

    Time: 3 ms (46.65%), Space: 13.7 MB (31.46%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,23 +1,23 @@
 class Solution {
-    public:
-    void getsub(vector<int> &nums,int i,vector<vector<int>> &ans,vector<int> a)
-    {
-      if(nums.size()==i)  
-      {
-          ans.push_back(a);
-          return;
-      }
-      getsub(nums,i+1,ans,a);
-      a.push_back(nums[i]);
-      getsub(nums,i+1,ans,a);
-      a.pop_back();
-      return;
-    }
 public:
+        void help(int i,vector<vector<int>> &ans,vector<int> &temp,vector<int> nums)
+        {
+            if(i==nums.size())
+            {
+                ans.push_back(temp);
+                return;
+            }
+            temp.push_back(nums[i]);
+            help(i+1,ans,temp,nums);
+            temp.pop_back();
+            help(i+1,ans,temp,nums);
+        }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> a;
-        getsub(nums,0,ans,a);
+        vector<int> temp;
+        help(0,ans,temp,nums);
         return ans;
+
+        
     }
 };
