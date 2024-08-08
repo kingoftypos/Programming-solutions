6091951610daf62b5df82ac1a790a542e1820d5d
Shreyansh <105883701+kingoftypos@users.noreply.github.com>
2024-08-08 18:31:01 +0530

    Time: 97 ms (51.75%), Space: 61.1 MB (54.29%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
+bool isTrue(string s,int end,int start)
+{
+        while(start<end)
+        {
+            if(s[start]!=s[end])
+            return false;
+            start++;
+            end--;
+        }
+        return true;
+}
+void solve(int ind,vector<vector<string>> &ans,vector<string>&temp,string s)
+{
+    if(ind==s.size())
+    {
+        ans.push_back(temp);
+        return;
+    }
+    for(int i=ind;i<s.size();i++)
+    {
+        if(isTrue(s,i,ind))
+        {
+            temp.push_back(s.substr(ind,i-ind+1));
+            solve(i+1,ans,temp,s);
+            temp.pop_back();
+        }
+    }
+}
     vector<vector<string>> partition(string s) {
         vector<vector<string>> ans;
         vector<string> temp;
         solve(0,ans,temp,s);
         return ans;
         
-    }
-    void solve(int index,vector<vector<string>> &ans,vector<string> &temp,string &s)
-    {
-        if(index==s.size())
-        {
-            ans.push_back(temp);
-            return;
-        }
-        for(int i=index;i<s.size();i++)
-        {
-            if(ispossible(s,index,i))
-            {
-                temp.push_back(s.substr(index,i-index+1));
-                solve(i+1,ans,temp,s);
-                temp.pop_back();
-            }
-        }
-    }
-    bool ispossible(string &s,int start,int end)
-    {
-        while(start<=end)
-        {
-            if(s[start++]!=s[end--])
-            return false;
-            
-        }
-        return true;
+        
     }
 };
